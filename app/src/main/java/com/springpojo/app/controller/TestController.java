57d2cb5c8cb5b501206cf352148a1cb95f0741c9
kiya <kiyamoon@daum.net>
2022-07-14 05:44:15 +0900

    first commit

---
+++ app/src/main/java/com/springpojo/app/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.springpojo.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	@GetMapping("test")
+	public String test(Model model) {
+		model.addAttribute("data", "new springboot!!");
+		return "test";
+	}
+}
